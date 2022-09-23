b99e7da6ad34c702e45b99bba4f6bf5bfdade021
Matthew <myee30@stuy.edu>
2022-09-23 13:13:24 -0400

    added 04_choose and krewes.py

---
+++ 04_choose/krewes.py
@@ -0,0 +1,32 @@
+import random
+"""
+James Yu
+SoftDev
+k04--randomDict - randomly picks a value from a dictionary
+2022-9-22
+Time Spent: .7 hours
+DISCO: Don't use existing function names like random
+       random.choice picks randomly from a list, string, or tuple
+       list() typecasts to a list
+QCC:
+OPS SUMMARY: krewes.keys() returns the keys in krewes.
+random.choice randomly selects one of the keys.
+l is the length of the selected key.
+Then, a random index is selected based on l.
+It returns a random devo.
+"""
+
+krewes = {
+           2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"],
+           7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+           8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "Wanying", "Kevin"]
+         }
+
+#choose between 2,7,8
+#Solution 1
+def randomDevo ():
+    randKey = random.choice(list(krewes.keys()))
+    l = len(krewes[randKey])
+    return krewes[randKey][random.randrange(0,l,1)]
+
+print(randomDevo())
