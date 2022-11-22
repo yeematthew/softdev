c6bdc4fd38fbdef74bb74df2eb8fda97b07234b6
Matthew Yee <myee30@stuy.edu>
2022-11-21 23:02:22 -0500

    barely functioning

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,12 +1,14 @@
-#Flying Karate Masters: Matthew Yee, Jeremy Kwok, David Chen
+#Spicy Vanilla: Matthew Yee, Jeremy Kwok, David Chen
 #SoftDev
 #K20 -- REST APIs
 #2022-11-21
-#time spent: ? hours
+#time spent: 1 hours
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
+import requests
+import json
 
 app = Flask(__name__)    #create Flask object
 
@@ -14,14 +16,27 @@ app = Flask(__name__)    #create Flask object
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
-@app.route('/')
-def index():
-    if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    return app.redirect(app.url_for('login'))
-
 @app.route('/', methods=['GET', 'POST'])
-def login():
-    error = ""
-    form_keys = request.form.keys()
-    print(request.form)
+def main():
+    key = "INSERT KEY HERE" #implement reading of key_nasa.txt?
+    response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={key}")
+    #print(response.status_code) # 200 means the request succeeded
+
+    #print(response.json()) #prints JSON response from API call
+
+    #text = json.dumps(response.json(), sort_keys=True, indent = 4) #formats JSON output into something more human-readable
+    #print(text)
+
+    temp = response.json()
+    #print(temp["url"])
+    image = temp["url"]
+    text = temp["explanation"]
+    #print(image)
+    #print(text)
+
+    return render_template('main.html', image = image, text = text)
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()
